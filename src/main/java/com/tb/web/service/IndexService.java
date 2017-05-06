ab40e2d6993cb65ff1b55872f6abcdc7a0577ec2
郝旭东 <haoxd@njhhet.com>
2017-05-06 22:06:19 +0800

    增加多种实现方式

--- src/main/java/com/tb/web/service/IndexService.java
+++ src/main/java/com/tb/web/service/IndexService.java
@@ -5,12 +5,17 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.tb.common.bean.EasyUIResult;
 import com.tb.common.bean.api.RespInfo;
+import com.tb.manager.pojo.Content;
 import com.tb.web.sys.contenst.httpContenst.HttpConstants;
 import com.tb.web.sys.httplient.ws.server.intf.HttpClientApiServerTools;
 import com.tb.web.util.xmlUtil.readAppXml;
@@ -75,7 +80,7 @@ public class IndexService {
 	}
 
 	/**
-	 * 获取主页小广告
+	 * 获取主页小广告(操作json对象)
 	 * 
 	 * @return
 	 */
@@ -108,6 +113,58 @@ public class IndexService {
 					data.add(map);
 				}
 				resp.setData(Mapper.writeValueAsString(data));// 序列化为JSON字符串
+				resp.setRespCode(HttpConstants.HttpStatusCode.OK);
+
+			} else {
+				resp.setRespDesc(respInfo.getRespDesc());
+				resp.setRespCode(HttpConstants.HttpStatusCode.SERVERERROR);
+			}
+		} catch (Exception e) {
+			resp.setRespDesc("请求小广告服务错误" + e);
+			resp.setRespCode(HttpConstants.HttpStatusCode.SERVERERROR);
+		}
+
+		return resp;
+
+	}
+	
+	
+	/**
+	 * 获取主页小广告（类对象操作）
+	 * 
+	 * 
+	 * @return
+	 */
+	public RespInfo queryIndexMinAdByClazz() {
+
+		RespInfo resp = new RespInfo();
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("categoryId", "5");
+		params.put("page", "1");
+		params.put("rows", "1");
+		List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+		try {			
+			RespInfo respInfo = this.apiServer.sendGetByParam(readAppXml.readAppXMLByNode("index", "indexPage"),
+					params);
+			if (HttpConstants.HttpStatusCode.OK.equals(respInfo.getRespCode())) {
+				EasyUIResult easyUIResult = EasyUIResult.formatToList(respInfo.getData().toString(), Content.class);
+				
+				List<Content> contentList =  (List<Content>) easyUIResult.getRows();
+				for (Content content : contentList) {
+
+					Map<String, Object> map = new LinkedHashMap<String, Object>();// 为保证和前端要求的顺序一致
+					map.put("width", 310);
+					map.put("height", 70);
+					map.put("src", content.getPic());
+					map.put("href", content.getUrl());
+					map.put("alt", content.getTitle());
+					map.put("widthB", 210);
+					map.put("heightB", 70);
+					map.put("srcB",  content.getPic());
+
+					data.add(map);
+				}
+				resp.setData(Mapper.writeValueAsString(data));// 序列化为JSON字符串 输出
 				resp.setRespCode(HttpConstants.HttpStatusCode.OK);
 
 			} else {
